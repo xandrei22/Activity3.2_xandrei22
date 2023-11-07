6ffa51cc510b233dd30719457cec96e9e5108181
xandrei22 <alexandrei1628@gmail.com>
2023-11-07 21:26:55 +0800

    “feat/select-heroes-archers”

--- hero.sql
+++ hero.sql
@@ -103,3 +103,9 @@ SELECT p.player_name, h.hero_name
 FROM public.player p
 JOIN public.hero h ON p.hero_id = h.hero_id
 WHERE h.is_active = true;
+
+-- List heroes classified as archers
+SELECT h.hero_name, c.class_name
+FROM public.hero h
+JOIN public.class c ON h.class_id = c.class_id
+WHERE c.class_name = 'Archer';
